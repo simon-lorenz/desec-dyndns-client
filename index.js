1c9571e0588f9626ac57bab6b67c4bbaf015d9bb
Simon Lorenz <mail@simon-lorenz.dev>
2021-08-08 01:43:33 +0200

    move source files to /src

--- index.js
+++ index.js
@@ -1,54 +1,11 @@
-const fetch = require('node-fetch')
 const { CronJob } = require('cron')
 
-const { logger } = require('./logger')
-const config = require('./config')
+const { run } = require('./src/client')
+const { logger } = require('./src/logger')
+const config = require('./src/config')
 
 const { cron } = config
 
 logger.info(`Initiating cron job with pattern "${cron}"`)
 
 new CronJob(cron, run, null, true, null, null, true)
-
-let currentIp = null
-
-async function run() {
-	try {
-		const publicIPv6 = await getPublicIPv6()
-
-		if (publicIPv6 === currentIp) {
-			logger.debug('Public IPv6 did not change')
-		} else {
-			logger.info(`Found new public IPv6: ${publicIPv6}`)
-			currentIp = publicIPv6
-			sendDynDNSUpdateRequest(publicIPv6)
-		}
-	} catch (err) {
-		logger.error({ err }, err.message)
-	}
-}
-
-async function getPublicIPv6() {
-	const { ipCheckUrl } = config
-
-	const res = await fetch(ipCheckUrl)
-
-	if (res.status === 200) {
-		return res.text()
-	} else {
-		throw new Error(`Could not get public IPv6 (${res.status})`)
-	}
-}
-
-async function sendDynDNSUpdateRequest(ip) {
-	const { server, domains, username, token } = config
-
-	const updateUrl = `${server}/?hostname=${domains.join(',')}&myip=${ip}`
-
-	await fetch(updateUrl, {
-		method: 'GET',
-		headers: { Authorization: `Basic ${Buffer.from(`${username}:${token}`).toString('base64')}` }
-	})
-
-	logger.info('DNS-Update successful!')
-}
---
+++ src/client.js
@@ -0,0 +1,49 @@
+const fetch = require('node-fetch')
+
+const { logger } = require('./logger')
+const config = require('./config')
+
+let currentIp
+
+async function run() {
+	try {
+		const publicIPv6 = await getPublicIPv6()
+
+		if (publicIPv6 === currentIp) {
+			logger.debug('Public IPv6 did not change')
+		} else {
+			logger.info(`Found new public IPv6: ${publicIPv6}`)
+			currentIp = publicIPv6
+			sendDynDNSUpdateRequest(publicIPv6)
+		}
+	} catch (err) {
+		logger.error({ err }, err.message)
+	}
+}
+
+async function getPublicIPv6() {
+	const { ipCheckUrl } = config
+
+	const res = await fetch(ipCheckUrl)
+
+	if (res.status === 200) {
+		return res.text()
+	} else {
+		throw new Error(`Could not get public IPv6 (${res.status})`)
+	}
+}
+
+async function sendDynDNSUpdateRequest(ip) {
+	const { server, domains, username, token } = config
+
+	const updateUrl = `${server}/?hostname=${domains.join(',')}&myip=${ip}`
+
+	await fetch(updateUrl, {
+		method: 'GET',
+		headers: { Authorization: `Basic ${Buffer.from(`${username}:${token}`).toString('base64')}` }
+	})
+
+	logger.info('DNS-Update successful!')
+}
+
+module.exports = { run }
