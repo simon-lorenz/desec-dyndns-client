29dd50773c6468034af852459d0898897a2cc4b6
Simon Lorenz <mail@simon-lorenz.dev>
2021-08-08 02:08:24 +0200

    daemon should append to log

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,6 @@ mkdir dist/config
 cp config/config.jsonc dist/config/
 
 echo '#!/bin/bash' >> dist/daemon.sh
-echo 'setsid ./desec-dyndns-client >$PWD/desec-dyndns-client.log 2>&1 < /dev/null &' >> dist/daemon.sh
+echo 'setsid ./desec-dyndns-client >> $PWD/desec-dyndns-client.log 2>&1 < /dev/null &' >> dist/daemon.sh
 
 chmod +x dist/daemon.sh
